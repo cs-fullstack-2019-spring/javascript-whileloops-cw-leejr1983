792a20d8871af4c70d0162a029d527b9882aee29
leejr1983 <leejr1983@yahoo.com>
2019-01-14 05:53:40 -0600

    classwork

---
+++ Problem3.js
@@ -0,0 +1,9 @@
+// CW3
+// Asks the user Enter a word. Enter 'q' to quit. Create a while loop that will not quit until they enter 'q'.
+// HINT: Use true condition so loop continues forever and break to exit the loop
+
+var i = "q";
+do{
+    n = prompt("Enter a word or enter 'q' to quit.");
+}
+while(i !== n);
